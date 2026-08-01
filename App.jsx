52b1b520c790d2343d1a7cb33b204730e38785c5
sandyjangir07-collab <sandyjangir07@gmail.com>
2026-08-01 17:00:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,52 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./hooks/useAuth";
+import { ToastProvider } from "./hooks/useToast";
+import { RequireAuth, RequireAdmin } from "./components/RouteGuards";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import EmployeeProfile from "./pages/EmployeeProfile";
+import Plots from "./pages/Plots";
+import Approvals from "./pages/Approvals";
+import CommissionSlabs from "./pages/CommissionSlabs";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <ToastProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              element={
+                <RequireAuth>
+                  <Layout />
+                </RequireAuth>
+              }
+            >
+              <Route path="/" element={<Home />} />
+              <Route path="/employees/:id" element={<EmployeeProfile />} />
+              <Route path="/plots" element={<Plots />} />
+              <Route
+                path="/approvals"
+                element={
+                  <RequireAdmin>
+                    <Approvals />
+                  </RequireAdmin>
+                }
+              />
+              <Route
+                path="/commission-slabs"
+                element={
+                  <RequireAdmin>
+                    <CommissionSlabs />
+                  </RequireAdmin>
+                }
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ToastProvider>
+    </AuthProvider>
+  );
+}
